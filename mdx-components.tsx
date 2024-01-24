f7e8dc7e490831c1ddcd3fb58e86c29a6e5f22bb
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-01-25 00:18:53 +0100

    Add more custom mdx components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -8,6 +8,29 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         h2: ({ children }) => (
             <h2 className="text-2xl font-bold my-4">{children}</h2>
         ),
+        h3: ({ children }) => (
+            <h3 className="text-xl font-bold my-4">{children}</h3>
+        ),
+        h4: ({ children }) => (
+            <h4 className="text-lg font-bold my-2">{children}</h4>
+        ),
+        a: ({ children, href }) => (
+            <a className="font-bold underline" href={href}>
+                {children}
+            </a>
+        ),
+        ol: ({ children }) => (
+            <ol className="list-decimal pl-8 my-2">{children}</ol>
+        ),
+        ul: ({ children }) => (
+            <ul className="list-disc pl-8 my-2">{children}</ul>
+        ),
+        li: ({ children }) => <li className="my-1">{children}</li>,
+        code: ({ children }) => (
+            <div className="my-4 p-4 rounded bg-zinc-800">
+                <code className="">{children}</code>
+            </div>
+        ),
         ...components,
     };
 }
