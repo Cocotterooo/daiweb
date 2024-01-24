77bf9ca440229bfc3ac872ba9cec2525eebb86d4
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-01-25 00:19:25 +0100

    everything in spanish

--- src/app/(common)/page.tsx
+++ src/app/(common)/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
     return (
         <div className="flex mx-40 items-center justify-center">
             <h1 className="text-6xl font-bold mr-10">
-                Delegación do Alumnado da <br />
-                Escola de Enxeñaría Industrial
+                Delegación de Alumnado de la <br />
+                Escuela de Ingeniería Industrial
             </h1>
         </div>
     );
