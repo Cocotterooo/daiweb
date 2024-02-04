35fcdbb5efba0aec3c969edb04a0e60da4141498
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-02-04 11:53:31 +0100

    remove next config clutter

--- next.config.mjs
+++ next.config.mjs
@@ -5,16 +5,6 @@ import rehypeSlug from "rehype-slug";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "media.vanityfair.com",
-                port: "",
-                pathname: "/photos/**",
-            },
-        ],
-    },
 };
 
 const withMDX = createMDX({
