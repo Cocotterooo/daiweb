a5b7d1f533c012445df8810338fae3e45a467b71
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-02-06 21:24:18 +0100

    Add database environment variables checks

--- src/database/client.ts
+++ src/database/client.ts
@@ -1,8 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./database.types";
 
-const databaseClient = createClient<Database>(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_KEY!
-);
-export default databaseClient;
+const databaseUrl = process.env.DATABASE_URL;
+const databaseKey = process.env.DATABASE_KEY;
+
+if (!databaseUrl) {
+    throw new Error("Missing DATABASE_URL environment variable");
+}
+
+if (!databaseKey) {
+    throw new Error("Missing DATABASE_KEY environment variable");
+}
+
+const database = createClient<Database>(databaseUrl, databaseKey);
+
+export default database;
